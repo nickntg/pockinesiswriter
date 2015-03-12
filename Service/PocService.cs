a463b31c319893f9554d0073e13ca5e1175fee69
n.bitounis <n.bitounis@pamediakopes.gr>
2015-03-13 00:03:04 +0200

    * Added service.

---
+++ Service/PocService.cs
@@ -0,0 +1,29 @@
+﻿using System.ServiceProcess;
+using log4net;
+using log4net.Config;
+using PocUnifiedLogWorkerCommon;
+
+namespace PocUnifiledLogService
+{
+    public partial class PocService : ServiceBase
+    {
+        public PocService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            XmlConfigurator.Configure();
+            var log = LogManager.GetLogger(typeof (PocService));
+            new Worker().WorkAsync();
+            log.Info("Started");
+        }
+
+        protected override void OnStop()
+        {
+            var log = LogManager.GetLogger(typeof(PocService));
+            log.Info("Stopped");
+        }
+    }
+}
